a94a5d7b62d7cb75bcd798233f46d99ec45dd45a
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-06-07 18:53:03 +0530

    Update App.js
    used useffect and cleanup function for removing the interval

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,23 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [time, setTime] = useState(0)
+
+    useEffect(()=>{
+      const idx = setInterval(()=>{
+        setTime((prev)=>prev+1)
+      },1000)
+  
+    return () => {
+     clearInterval(idx)
+    }
+  }, [])
+  
+  
+  console.log(time)
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>You have on this page from {time} seconds</h1>
     </div>
   )
 }
